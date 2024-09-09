bf500347033f18241b916e202b73990e04a968ec
hangilzzang <gkrwhddlwjqrl@gmail.com>
2024-09-09 20:26:32 +0900

    20240909

--- Assets/Script/TouchToStart.cs
+++ Assets/Script/TouchToStart.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TouchToStart : MonoBehaviour
 {
-    public GameObject touch;
     void Start()
     {
         EventManager.instance.MouseDownEvent += GameStart; // 이벤트 등록
@@ -22,7 +21,6 @@ public class TouchToStart : MonoBehaviour
         {
             GameManager.instance.gameState = GameManager.GameState.Ready; // 상태변경
             EventManager.instance.TriggerStateChanged(); // 상태변경 이벤트 트리거
-            touch.SetActive(false);
             gameObject.SetActive(false); // 메인화면 ui 제거
         }
     }
